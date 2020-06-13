428ef53b982bca83861d332c3bc56e8c55bd8518
oknagisa <rs40416996@gmail.com>
2020-06-13 19:04:03 +0530

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 echo 'Deploying[1/4]: Building...'
-yarn build
+yarn && yarn build
 echo 'Deploying[2/4]: Remove prev build...'
-rm -rf ../okrohan.github.io/bounce/*
+rm -rf ../oknagisa.github.io/boink/*
 echo 'Deploying[3/4]: Copying new build...'
-cp ./dist/* ../okrohan.github.io/bounce -f -r
+cp ./dist/* ../oknagisa.github.io/boink -f -r
 echo 'Deploying[4/4]: Commit to github'
-cd ../okrohan.github.io && git add . && git commit -m "Script: Updated bounce build" && git push origin HEAD
+cd ../oknagisa.github.io && git add . && git commit -m "Script: Updated boink build" && git push origin HEAD
 echo 'Done!'
